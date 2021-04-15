1182bb54ec76cd2268035fef068e08167c578002
OG-netizen <70165988+OG-netizen@users.noreply.github.com>
2021-04-15 14:29:21 +0200

    Update case_study_his.ino

--- case_study_his.ino
+++ case_study_his.ino
@@ -23,11 +23,11 @@ void setup() {
 }
 
 void loop() {
-  float distance1 = distance(trigPin, echoPin);
-  float distance2 = distance(trigPin2, echoPin2);
-  float Tone = map(distance1, 5, 20, 40, 3000);
-  float Volume = map(distance2, 5, 20, 0, 255);
-  Tone = constrain(Tone, 40, 3000);
+  float distance1 = distance(trigPin, echoPin); // eerste ultrasoon is om de toonhoogte te veranderen
+  float distance2 = distance(trigPin2, echoPin2); // tweede ultrasoon is om de volume te veranderen
+  float Tone = map(distance1, 5, 20, 40, 3000); // Map de toonhoogte
+  float Volume = map(distance2, 5, 20, 0, 255); // Map de volume
+  Tone = constrain(Tone, 40, 3000); // Alle waardes zit in een bepaalde bereik
   Volume = constrain(Volume, 0, 255);
 
   for(int i = 0; i < arrayLength - 1; i++) {
@@ -44,12 +44,12 @@ void loop() {
   for(int i = 0; i < arrayLength; i++) {
     total += tones[i];
   }
-  float averageTone = total / arrayLength;
+  float averageTone = total / arrayLength; // De gemmidelde toon berekenen
   total = 0;
   for(int i = 0; i < arrayLength; i++) {
     total += volumes[i];
   }
-  float averageVolume = total / arrayLength;
+  float averageVolume = total / arrayLength; // De gemmidelde volume berekenen
 
   Serial.print("Tone: ");
   Serial.println(averageTone);
